c891d5696f7516a946cfb69a06ad788785c92afd
Gia Yến <yenddgce180945@fpt.edu.vn>
2026-06-15 01:43:56 +0700

    [ECS-100] Implement system dashboard endpoint for viewing data

--- ECSBackendAPI/ECSBackendAPI/ECS.Application/DependencyInjection.cs
+++ ECSBackendAPI/ECSBackendAPI/ECS.Application/DependencyInjection.cs
@@ -9,10 +9,11 @@ using ECS.Application.Services.ClinicAdminManagementServices.ClinicProfileServic
 using ECS.Application.Services.ClinicAdminManagementServices.CreateStaffAccountServices;
 using ECS.Application.Services.ClinicAdminManagementServices.EditClinicProfileServices;
 using ECS.Application.Services.ClinicAdminManagementServices.ViewListStaffAccountsServices;
-using ECS.Application.Services.ClinicDoctorDiscoveryService.SearchClinicDoctorServices;
 using ECS.Application.Services.ClinicDoctorDiscoveryService.RegisterClinicApplicationServices;
+using ECS.Application.Services.ClinicDoctorDiscoveryService.SearchClinicDoctorServices;
 using ECS.Application.Services.SystemAdminServices.AdminSystemDeleteClinicServices;
 using ECS.Application.Services.SystemAdminServices.AdminSystemGetClinicDetailsServices;
+using ECS.Application.Services.SystemAdminServices.AdminSystemGetDashboardServices;
 using ECS.Application.Services.SystemAdminServices.AdminSystemUpdateClinicServices;
 using ECS.Application.Services.SystemAdminServices.ApproveClinicApplicationServices;
 using ECS.Application.Services.SystemAdminServices.ClinicManagementServices;
@@ -54,6 +55,7 @@ public static class DependencyInjection
         services.AddScoped<IUpdateClinicService, UpdateClinicService>();
         services.AddScoped<IGetClinicDetailsService, GetClinicDetailsService>();
         services.AddScoped<IAdminSystemDeleteClinicService, AdminSystemDeleteClinicService>();
+        services.AddScoped<IAdminSystemGetDashboardService, AdminSystemGetDashboardService>();
 
         // ── FluentValidation ──────────────────────────────────
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
