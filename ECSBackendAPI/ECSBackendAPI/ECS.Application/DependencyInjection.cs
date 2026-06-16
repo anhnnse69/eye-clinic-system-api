91448fd39220b1bed8e4dd865fb7969516c292fa
Tuấn Anh <tuananh123123lon@gmail.com>
2026-06-16 16:13:08 +0700

    [ECS-139] feat: Implement Create patient profile API for patient role

--- ECSBackendAPI/ECSBackendAPI/ECS.Application/DependencyInjection.cs
+++ ECSBackendAPI/ECSBackendAPI/ECS.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using ECS.Application.Services.AuthServices.ChangePasswordServices;
 using ECS.Application.Services.AuthServices.ForgotPasswordServices;
 using ECS.Application.Services.AuthServices.LoginServices;
 using ECS.Application.Services.AuthServices.RegisterServices;
@@ -18,9 +19,10 @@ using ECS.Application.Services.ClinicAdminManagementServices.ViewListStaffAccoun
 using ECS.Application.Services.ClinicDoctorDiscoveryService.RegisterClinicApplicationServices;
 using ECS.Application.Services.ClinicDoctorDiscoveryService.SearchClinicDoctorServices;
 using ECS.Application.Services.ClinicDoctorDiscoveryService.ViewClinicFeedbacksServices;
-using ECS.Application.Services.PatientProfileManagementServices.GetPatientProfilesServices;
 using ECS.Application.Services.ClinicDoctorDiscoveryService.ViewClinicProfileServices;
 using ECS.Application.Services.ClinicDoctorDiscoveryService.ViewDoctorSlotsServices;
+using ECS.Application.Services.PatientProfileManagementServices.CreatePatientProfileServices;
+using ECS.Application.Services.PatientProfileManagementServices.GetPatientProfilesServices;
 using ECS.Application.Services.SystemAdminServices.AdminSystemDeleteClinicServices;
 using ECS.Application.Services.SystemAdminServices.AdminSystemGetClinicDetailsServices;
 using ECS.Application.Services.SystemAdminServices.AdminSystemGetDashboardServices;
@@ -30,7 +32,6 @@ using ECS.Application.Services.SystemAdminServices.ClinicManagementServices;
 using ECS.Application.Services.SystemAdminServices.ClinicRegisterServices;
 using ECS.Application.Services.SystemAdminServices.RejectClinicApplicationServices;
 using ECS.Application.Services.SystemAdminServices.ReviewClinicRegisterServices;
-using ECS.Application.Services.AuthServices.ChangePasswordServices;
 using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -76,6 +77,7 @@ public static class DependencyInjection
         services.AddScoped<IViewClinicServicesService, ViewClinicServicesService>();
         services.AddScoped<IViewClinicFeedbacksService, ViewClinicFeedbacksService>();
         services.AddScoped<IChangePasswordService, ChangePasswordService>();
+        services.AddScoped<ICreatePatientProfileService, CreatePatientProfileService>();
 
         // ── FluentValidation ──────────────────────────────────
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
---
+++ ECSBackendAPI/ECSBackendAPI/ECS.Application/Services/PatientProfileManagementServices/CreatePatientProfileServices/CreatePatientProfileResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace ECS.Application.Services.PatientProfileManagementServices.CreatePatientProfileServices
+{
+    /// <summary>
+    /// Response data entity conveying confirmation details of the provisioned patient profile.
+    /// </summary>
+    public class CreatePatientProfileResponse
+    {
+        public Guid PatientProfileId { get; set; }
+        public string FullName { get; set; } = null!;
+        public string Relationship { get; set; } = null!;
+        public string CreatedAt { get; set; } = null!;
+    }
+}
