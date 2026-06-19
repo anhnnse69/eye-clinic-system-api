4c33a597b2d6c0213c81a33087916213e7a8da42
nhantruong7704 <nhantruong7704@gmail.com>
2026-06-19 15:40:08 +0700

    [ECS-229] - Create Medicine Catalog

---
+++ ECSBackendAPI/ECSBackendAPI/ECS.Application/Services/ClinicAdminManagementServices/MedicineCatalogServices/Create/CreateMedicineCatalogResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECS.Application.Services.ClinicAdminManagementServices.MedicineCatalogServices.Create
+{
+    /// <summary>
+    /// Response payload returning the newly created medicine catalog unique identifier.
+    /// </summary>
+    public class CreateMedicineCatalogResponse
+    {
+        /// <summary>
+        /// Gets or sets the generated unique identifier of the medicine catalog entry.
+        /// </summary>
+        public string Id { get; set; } = null!;
+    }
+}
