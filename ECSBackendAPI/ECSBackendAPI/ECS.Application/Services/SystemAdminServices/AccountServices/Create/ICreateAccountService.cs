37160482c0f39a81acc211e130f3a82367f9f75d
nhantruong7704 <nhantruong7704@gmail.com>
2026-06-21 19:47:20 +0700

    [ECS-268] - Create Account

---
+++ ECSBackendAPI/ECSBackendAPI/ECS.Application/Services/SystemAdminServices/AccountServices/Create/ICreateAccountService.cs
@@ -0,0 +1,20 @@
+﻿using ECS.Application.Common.Response;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECS.Application.Services.SystemAdminServices.AccountServices.Create
+{
+    /// <summary>
+    /// Service contract defining operations for registering or creating system users accounts.
+    /// </summary>
+    public interface ICreateAccountService
+    {
+        /// <summary>
+        /// Executes the domain operational flow logic to parse parameters, evaluate collisions, and register account states.
+        /// </summary>
+        /// <param name="request">The data transport model payload containing new identity configurations.</param>
+        /// <returns>A standard api response tracking result envelope structures.</returns>
+        Task<ApiResponse<CreateAccountResponse>> Process(CreateAccountRequest request);
+    }
+}
